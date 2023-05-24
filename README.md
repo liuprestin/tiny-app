ff53343031376d07ea64663f5cf39def8f34df4d
Prestin Liu <liuprestin@proton.me>
2023-05-23 18:03:55 -0700

    adding axios as an HTTP client

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 
 
-Dependencies
+## Dev server run
+
+`npm start`
+
+## Dependencies
 - express js
 - EJS template engine https://ejs.co/
+- (dev) nodemon https://github.com/remy/nodemon
--- express_server.js
+++ express_server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const { generateRandomString } = require("./util.js");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  
+  //res.json(req.body);
+  let longURL = res.body.longURL;
+  urlDatabase[generateRandomString()] = longURL;
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,10 +31,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
-  });
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
