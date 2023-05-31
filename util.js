e98ef0a7be30ba6ba7b35c8adbbbc78a42cd3bb9
Prestin Liu <liuprestin@proton.me>
2023-05-30 18:42:31 -0700

    add ulitity function to check the user database object for email

--- util.js
+++ util.js
@@ -1,7 +1,20 @@
-//utility function to generate a random string 
+//utility function to generate a random string
 // right now the function has a hard coded length
-function generateRandomString(){
-    return Math.random().toString(20).substring(2, 15);
+function generateRandomString() {
+  return Math.random().toString(20).substring(2, 15);
 }
 
-module.exports = { generateRandomString };
+//utility function to traverse through the user object (see express_server.js)
+// and check if the given email is present
+// will return true or false
+function userEmailSearch(collection, email) {
+  for (let [key, value] of Object.entries(collection)) {
+    console.log(collection[key].email);
+    if (collection[key].email == email) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = { generateRandomString, userEmailSearch };
